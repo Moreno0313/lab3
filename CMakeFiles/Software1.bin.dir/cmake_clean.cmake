145f4e1bbd69a6528f47fb05fc79fa5c130c8345
Moreno0313 <102369007+Moreno0313@users.noreply.github.com>
2022-05-11 20:09:24 -0500

    Add files via upload

---
+++ CMakeFiles/Software1.bin.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Software1.bin.dir/src/config_tty.cpp.o"
+  "CMakeFiles/Software1.bin.dir/src/funciones_gr1.cpp.o"
+  "CMakeFiles/Software1.bin.dir/src/main.cpp.o"
+  "CMakeFiles/Software1.bin.dir/src/participant.cpp.o"
+  "build/Software1.bin.pdb"
+  "build/Software1.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Software1.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Software1.build.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Software1.build.dir/src/config_tty.cpp.o"
+  "CMakeFiles/Software1.build.dir/src/funciones_gr1.cpp.o"
+  "CMakeFiles/Software1.build.dir/src/main.cpp.o"
+  "CMakeFiles/Software1.build.dir/src/participant.cpp.o"
+  "build/Software1.build.pdb"
+  "build/Software1.build"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Software1.build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
